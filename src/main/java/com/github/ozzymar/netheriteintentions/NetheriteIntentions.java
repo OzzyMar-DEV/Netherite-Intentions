c386e1b705de31448cb1102a23485047ad73a177
Oscar <ozzy.coding@hotmail.com>
2021-08-08 09:09:17 +0100

    Release 1.2 & refactor code

--- src/main/java/com/github/ozzymar/netheriteintentions/NetheriteIntentions.java
+++ src/main/java/com/github/ozzymar/netheriteintentions/NetheriteIntentions.java
@@ -3,16 +3,11 @@ package com.github.ozzymar.netheriteintentions;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.entity.Player;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.inventory.PlayerInventory;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 import org.bukkit.scheduler.BukkitRunnable;
-
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 public class NetheriteIntentions extends JavaPlugin {
 
@@ -34,25 +29,24 @@ public class NetheriteIntentions extends JavaPlugin {
 
                             ItemStack[] armor = player.getInventory().getArmorContents();
 
-                            for(ItemStack item : armor)
-                            {
-                                if(item==null)
+                            for (ItemStack item : armor) {
+                                if (item == null)
                                     continue;
 
-                                if(!isWearingHelm)
+                                if (!isWearingHelm)
                                     isWearingHelm = item.getType().equals(Material.NETHERITE_HELMET);
 
-                                if(!isWearingChest)
+                                if (!isWearingChest)
                                     isWearingChest = item.getType().equals(Material.NETHERITE_CHESTPLATE);
 
-                                if(!isWearingLegs)
+                                if (!isWearingLegs)
                                     isWearingLegs = item.getType().equals(Material.NETHERITE_LEGGINGS);
 
-                                if(!isWearingBoots)
+                                if (!isWearingBoots)
                                     isWearingBoots = item.getType().equals(Material.NETHERITE_BOOTS);
                             }
 
-                            if(!isWearingHelm || !isWearingChest || !isWearingLegs|| !isWearingBoots)
+                            if (!isWearingHelm || !isWearingChest || !isWearingLegs || !isWearingBoots)
                                 return;
 
                             new PotionEffect(
